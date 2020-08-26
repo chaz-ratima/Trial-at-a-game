fec4473726ec2be27e5b1b578b657d48f2208910
chaz-ratima <chaz@ChazPC.localdomain>
2020-08-26 17:54:59 +1200

    add working react and localhost

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+
+const App = () => {
+    return (
+        <>
+        <h1>React Time</h1>
+        </>
+    )
+}
+
+export default App
--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
-const router = require('./routes/test')
+const path = require ('path')
 const express = require('express')
-const fs = require('fs')
-const hbs = require('express-handlebars')
+
 const server = express()
 
-// Middleware
-server.engine('hbs', hbs({
-  defaultLayout: 'main',
-  extname: 'hbs'
-}))
-server.set('view engine', 'hbs')
-server.use(express.static('public'))
-server.use(express.urlencoded({extended: false}))
-server.use(router)
+server.use(express.static(path.join(__dirname, '../public')))
 
 module.exports = server
+
+
+// const router = require('./routes/test')
+// const fs = require('fs')
+// const hbs = require('express-handlebars')
+
+
+// Middleware
+
+// server.engine('hbs', hbs({
+//   defaultLayout: 'main',
+//   extname: 'hbs'
+// }))
+// server.set('view engine', 'hbs')
+// server.use(express.static('public'))
+// server.use(express.urlencoded({extended: false}))
+// server.use(router)
